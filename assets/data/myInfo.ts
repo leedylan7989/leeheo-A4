72a1757eb292430a2465efd4f15e9dc5f873d78d
leedylan7989 <51422675+leedylan7989@users.noreply.github.com>
2019-08-09 01:41:36 -0400

    Add files via upload

---
+++ assets/data/myInfo.ts
@@ -0,0 +1,6 @@
+import {leeheo} from '../../app/leeheo';
+
+export const MYINFO : leeheo = {
+    studentnumber: 991280638, name: "Heon Lee", loginName: "leeheo",
+    campus:"Trafalgar Campus", assignmentTitle: "Assignment 4"
+  };
